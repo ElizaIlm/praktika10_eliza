8ef0d8f699446e9456c66f628e3fed6a9483b78a
ilmasheva-eliza <ilmeliza36@gmai.com>
2025-10-24 23:11:36 +0500

    программирование окна MainWindow.Ильмашева

--- pr10elik/MainWindow.xaml.cs
+++ pr10elik/MainWindow.xaml.cs
@@ -20,17 +20,45 @@ namespace pr10elik
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Classes.Passport> Passports = new List<Classes.Passport>();
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
         }
+
+        private void Update(object sender, RoutedEventArgs e)
+        {
+            if (lv_passport.SelectedIndex > -1)
+                new Windows.Add(lv_passport.SelectedItem as Classes.Passport).ShowDialog();
+            else
+                MessageBox.Show("Выберите паспорт");
+        }
+
         private void Add(object sender, RoutedEventArgs e)
         {
-
+            new Windows.Add(null).ShowDialog();
         }
+
         private void Delete(object sender, RoutedEventArgs e)
         {
-
+            if (lv_passport.SelectedIndex > -1)
+            {
+                Passports.Remove(lv_passport.SelectedItem as Classes.Passport);
+                LoadPassports();
+            }
+            else
+                MessageBox.Show("Выберите паспорт");
+        }
+        public void LoadPassports()
+        {
+            lv_passport.Items.Clear();
+            foreach (Classes.Passport Passport in Passports)
+            {
+                lv_passport.Items.Add(Passport);
+            }
         }
     }
 }
+
